fc18c00fd2939232f00b16366322704d55095716
Michael Tremel <itrekkie@gmail.com>
2016-12-28 14:43:24 -0700

    switch IR to 64 bits

--- src/ir.cc
+++ src/ir.cc
@@ -9,9 +9,5 @@ IR::IR(Op op, uint16_t a) : v(0) { set(op, a, 0); }
 IR::IR(Op op, uint16_t a, int16_t b) : v(0) { set(op, a, b); }
 
 void IR::set(Op op, uint16_t a, int16_t b) {
-  assert(a < (1 << 14));
-  assert(b < (1 << (14 - 1)));
-
-  const uint32_t arg = a << 14 | (uint16_t)(0x3fff & b);
-  v = op << 28 | arg;
+  v = ((uint64_t)op << 32) | (((uint32_t)a << 16) | ((uint16_t)b));
 }
--- src/ir.h
+++ src/ir.h
@@ -17,7 +17,7 @@ enum Op {
   Clear,
   Hlt
 };
-static_assert(Op::Hlt <= (1 << 4), "4-bits opcode overflow");
+static_assert(Op::Hlt <= (1l << 32), "4-bits opcode overflow");
 
 static const std::map<Op, std::string> Op2Str = {
     {Op::IncPtr, "IncPtr"},   {Op::DecPtr, "DecPtr"},
@@ -31,32 +31,26 @@ public:
   // 32-28, 28-14, 14-0
   // Op     A      B
   // 4      14     14
-  uint32_t v;
+  uint64_t v;
 
   explicit IR(Op op);
   IR(Op op, uint16_t a);
   IR(Op op, uint16_t a, int16_t b);
 
-  Op getOp() const { return (Op)(uint8_t)(v >> 28); }
+  Op getOp() const { return (Op)(uint32_t)(v >> 32); }
   uint16_t getA() const {
-    uint32_t arg = 0xfffffff & v;
-    return arg >> 14;
+    uint32_t arg = v;
+    return arg >> 16;
   }
   int16_t getB() const {
-    uint32_t arg = 0xfffffff & v;
-    uint16_t masked = 0x3fff & arg;
-
-    // http://graphics.stanford.edu/~seander/bithacks.html#VariableSignExtend
-    // Public Domain
-    const auto signBit = 1 << (14 - 1);
-    return (masked ^ signBit) - signBit;
+    uint32_t arg = v;
+    return (int16_t)arg;
   }
 
 private:
   void set(Op op, uint16_t a, int16_t b);
 };
 
-static_assert(sizeof(IR) == sizeof(uint32_t),
-              "IR must be the size of uint32_t");
+static_assert(sizeof(IR) == sizeof(uint64_t), "IR must be 64 bits");
 
 #endif // IR_H
