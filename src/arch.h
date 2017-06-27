9eee9889e62c86855a0df383a8cf6393798776f2
Michael Tremel <itrekkie@gmail.com>
2017-06-26 19:26:01 -0700

    add basic arm build

--- src/arch.h
+++ src/arch.h
@@ -4,6 +4,8 @@
 #if defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) ||             \
     defined(_M_AMD64)
 #define BF_JIT_X64
+#elif defined(__arm__) || defined(__arm) || defined(__ARM__) || defined(__ARM)
+#define BF_JIT_ARM
 #endif
 
 #endif // ARCH_H
